d93d3599019bcfd9719ee549781489db636484dd
Guido <guidoxenofonte@gmail.com>
2026-02-27 23:47:35 -0300

    add idempotency.py, formatted migrations.sql and queries.py

--- notifications/server/idempotency.py
+++ notifications/server/idempotency.py
@@ -1,5 +1,13 @@
-import asyncio
-import asyncpg
+async def checkIfProcessed(connection, event_id) -> bool:
+    response = await connection.fetchrow(
+        """SELECT event_id FROM processed_events WHERE event_id = $1""", event_id
+    )
 
-from notifications.server.db.connection import getConnectionPool
+    return True if response else False
 
+
+async def markAsProcessed(connection, event_id) -> None:
+    await connection.execute(
+        """INSERT INTO processed_events (event_id) VALUES ($1) ON CONFLICT (event_id) DO NOTHING""",
+        event_id,
+    )
